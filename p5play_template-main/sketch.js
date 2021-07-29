6befac70132c6bf1341f2131650e162883c44b31
YashitaT22 <88138238+YashitaT22@users.noreply.github.com>
2021-07-29 17:28:34 +0800

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,21 @@
+var ball1 ;
+
+function setup() {
+
+  createCanvas(1350,750);
+  ball1 = createSprite (20, 20, 20, 20);
+}
+
+function draw() 
+{
+  background("lightblue");
+drawSprites();
+
+ball1.shapeColor = "white";
+}
+
+
+
+
+
+
